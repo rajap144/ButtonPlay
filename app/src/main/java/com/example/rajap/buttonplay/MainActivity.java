f5ca836cbb6d879eda294af7b74ca5b1489867fa
rajap144 <rajap144@gmail.com>
2018-01-11 21:55:29 +0530

    added some code for srolling

--- app/src/main/java/com/example/rajap/buttonplay/MainActivity.java
+++ app/src/main/java/com/example/rajap/buttonplay/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rajap.buttonplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,15 +27,21 @@ public class MainActivity extends AppCompatActivity
 		userInput = findViewById(R.id.editText);
 		button = findViewById(R.id.button);
 		textView = findViewById(R.id.textView2);
+		textView.setText("");
+		textView.setMovementMethod(new ScrollingMovementMethod());
 		View.OnClickListener myOnClickListener = new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View view)
 			{
 				numTimesClicked += 1;
-				String result = "\n The Button got tapped " + numTimesClicked + "times.";
+				String result = " The Button got tapped " + numTimesClicked + " time";
 				textView.append(result);
-
+				if(numTimesClicked != 1)
+				{
+					result += "s";
+				}
+				result += "\n";
 			}
 		};
 		button.setOnClickListener(myOnClickListener);
